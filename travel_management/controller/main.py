548a18b8885adb0c474c7f03d71f1022f701e211
abinsuresh24 <abin.suresh24@gmail.com>
2023-06-21 15:32:19 +0530

    Travel management

--- travel_management/controller/main.py
+++ travel_management/controller/main.py
@@ -111,13 +111,3 @@ class CreateCustomer(http.Controller):
         request.env['res.partner'].sudo().create(partner)
         return request.render("travel_management.customer_success",
                               {'type': 'ir.actions.client', 'tag': 'reload'})
-
-
-# class Events(http.Controller):
-#    @http.route(['/total_product_sold'], type="json", auth="public")
-#    def sold_total(self):
-#        sale_obj = request.env['sale.order'].sudo().search([
-#            ('state', 'in', ['done', 'sale']),
-#        ])
-#        total_sold = sum(sale_obj.mapped('order_line.product_uom_qty'))
-#        return total_sold
